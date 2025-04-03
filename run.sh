66cba07d49e0f8bfe0574b9ba871fad4c3f5c0cc
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-04-03 13:49:42 +0100

    install packages before running

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	# install linux packages
+	sudo apt get npm
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # install macOS packages
+	brew install node
+	brew install npm
+
 npm install
 node js/server.js & sleep 2
 python3 browser.py
